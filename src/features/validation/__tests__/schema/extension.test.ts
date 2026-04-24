4b3de764252dd6f46be37cf70e44ed325ec861b6
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-04-24 23:55:12 +0300

    fixed lack of test coverage near validation/extension.ts

--- src/features/validation/__tests__/schema/extension.test.ts
+++ src/features/validation/__tests__/schema/extension.test.ts
@@ -1,6 +1,15 @@
 import { describe, expect, it } from 'vitest'
 
-import { defineExtension, string, type StringSchema } from '@/features/validation'
+import {
+  boolean,
+  defineBooleanExtension,
+  defineExtension,
+  defineNumberExtension,
+  defineStringExtension,
+  number,
+  string,
+  type StringSchema,
+} from '@/features/validation'
 
 const email = defineExtension<StringSchema, [message?: string]>(
   (message = 'Invalid email') =>
@@ -31,5 +40,41 @@ describe('defineExtension', () => {
         },
       ])
     }
+  })
+
+  it('build string extensions', () => {
+    const hex = defineStringExtension<[message?: string]>(
+      (message = 'Invalid hex color') =>
+        schema =>
+          schema.pattern(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, message)
+    )
+    expect(string().trim().use(hex()).safeParse('#fff')).toMatchObject({
+      ok: true,
+      value: '#fff',
+    })
+  })
+
+  it('build number extension', () => {
+    const birthYear = defineNumberExtension<[message?: string]>(
+      (message = 'Invalid birth year') =>
+        schema =>
+          schema.min(1900, message).max(new Date().getFullYear(), message)
+    )
+    expect(number().coerce().use(birthYear()).safeParse('2000')).toMatchObject({
+      ok: true,
+      value: 2000,
+    })
+  })
+
+  it('build boolean extension', () => {
+    const isEvening = defineBooleanExtension<[message?: string]>(
+      (message = 'Invalid evening flag') =>
+        schema =>
+          schema.refine(value => value === true, { message })
+    )
+    expect(boolean().use(isEvening()).safeParse(true)).toMatchObject({
+      ok: true,
+      value: true,
+    })
   })
 })
