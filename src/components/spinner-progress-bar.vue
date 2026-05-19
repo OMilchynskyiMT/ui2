c2f5330f53f70668c47514719c76d15c18cc205d
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-05-19 22:13:56 +0300

    add spinner component

---
+++ src/components/spinner-progress-bar.vue
@@ -0,0 +1,131 @@
+<template>
+  <svg
+    role="progressbar"
+    :class="{ indeterminate }"
+    :height="size"
+    :viewBox="`0 0 ${viewBoxSize} ${viewBoxSize}`"
+    :width="size"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-valuemax="100"
+    aria-valuemin="0"
+  >
+    <circle
+      :cx="halfBoxSize"
+      :cy="halfBoxSize"
+      :r="halfBoxSize - strokeWidth / 2"
+      :stroke-width="strokeWidth"
+      class="track"
+      fill="none"
+    />
+    <circle
+      :cx="halfBoxSize"
+      :cy="halfBoxSize"
+      :r="halfBoxSize - strokeWidth / 2"
+      :stroke-dashoffset="strokeDashOffset"
+      :stroke-width="strokeWidth"
+      class="progress"
+      fill="none"
+      pathLength="100"
+      stroke="currentColor"
+      stroke-dasharray="100"
+    />
+    <text
+      v-if="value !== undefined && !indeterminate"
+      :x="halfBoxSize"
+      :y="(viewBoxSize + strokeWidth) / 2"
+      dominant-baseline="middle"
+      fill="currentColor"
+      font-size="1rem"
+      text-anchor="middle"
+    >
+      {{ value }}
+    </text>
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const viewBoxSize = 48
+const halfBoxSize = viewBoxSize / 2
+const {
+  size = '3rem',
+  strokeWidth = 2,
+  value,
+  indeterminate = false,
+} = defineProps<{ size?: string; strokeWidth?: number; value?: number; indeterminate?: boolean }>()
+
+const strokeDashOffset = computed(() => 100 - Math.min(100, Math.max(0, value ?? 0)))
+</script>
+
+<style scoped>
+svg {
+  --size: 3rem;
+
+  position: relative;
+  inline-size: var(--size);
+  block-size: var(--size);
+  display: inline-grid;
+  place-items: center;
+
+  &.indeterminate {
+    transform: rotate(0deg);
+    animation: material-spinner-rotate 1.4s linear infinite;
+
+    & > .progress {
+      animation: material-spinner-dash 1.4s ease-in-out infinite;
+    }
+  }
+
+  & > circle.track {
+    stroke: color-mix(in oklch, currentColor, transparent 90%);
+  }
+
+  & > circle {
+    transition-property: stroke-dashoffset, stroke-dasharray, stroke-width, transform;
+    transition-duration: var(--duration-lg);
+    transition-timing-function: var(--bezier-magnetic);
+    will-change: stroke-dashoffset, stroke-dasharray, transform;
+
+    transform: rotate(-90deg);
+    transform-origin: center;
+  }
+}
+
+@keyframes material-spinner-rotate {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes material-spinner-dash {
+  0% {
+    stroke-dasharray: 1 100;
+    stroke-dashoffset: 0;
+  }
+
+  50% {
+    stroke-dasharray: 80 100;
+    stroke-dashoffset: -20;
+  }
+
+  100% {
+    stroke-dasharray: 1 100;
+    stroke-dashoffset: -101;
+  }
+}
+
+@media (prefers-reduced-motion: reduce) {
+  svg {
+    animation: none;
+
+    & > circle {
+      transition-duration: 1ms;
+    }
+
+    & > .progress {
+      animation: none;
+    }
+  }
+}
+</style>
