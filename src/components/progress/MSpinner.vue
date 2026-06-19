f577834938e15041f79277f09b3316ed8bbf808d
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-06-19 22:00:36 +0300

    update MSpinner component

--- src/components/progress/MSpinner.vue
+++ src/components/progress/MSpinner.vue
@@ -1,56 +1,30 @@
 <template>
   <svg
-    ref="svg"
     role="progressbar"
     :aria-valuenow="indeterminate ? undefined : normalizedValue"
     :class="{ indeterminate }"
-    :height="size"
     :viewBox="`0 0 ${viewBoxSize} ${viewBoxSize}`"
-    :width="size"
     aria-valuemax="100"
     aria-valuemin="0"
     xmlns="http://www.w3.org/2000/svg"
   >
+    <circle v-if="showTrack" :cx="halfBoxSize" :cy="halfBoxSize" :r="radius" class="track" fill="none" />
     <circle
-      v-if="showTrack"
-      :cx="halfBoxSize"
-      :cy="halfBoxSize"
-      :r="halfBoxSize - strokeWidth / 2"
-      :stroke-width="strokeWidth"
-      class="track"
-      fill="none"
-    />
-    <circle
-      :cx="halfBoxSize"
-      :cy="halfBoxSize"
-      :r="halfBoxSize - strokeWidth / 2"
+      :r="radius"
       :stroke-dashoffset="strokeDashOffset"
-      :stroke-width="strokeWidth"
       class="progress"
-      fill="none"
       pathLength="100"
-      stroke="currentColor"
       stroke-dasharray="100"
     />
-    <text
-      :font-size="fontSize"
-      :x="halfBoxSize"
-      :y="(viewBoxSize + strokeWidth) / 2"
-      dominant-baseline="middle"
-      fill="currentColor"
-      text-anchor="middle"
-    >
+    <text v-if="value !== undefined" :x="halfBoxSize" :y="halfBoxSize">
       {{ normalizedValue }}
     </text>
   </svg>
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, useTemplateRef, watch } from 'vue'
+import { computed } from 'vue'
 
-const svg = useTemplateRef<SVGElement>('svg')
-const viewBoxSize = 48
-const halfBoxSize = viewBoxSize / 2
 const {
   size = '3rem',
   fontSize = '1rem',
@@ -67,32 +41,37 @@ const {
   showTrack?: boolean
 }>()
 
+const initialBoxSize = 48
+const viewBoxSize = initialBoxSize + strokeWidth * 2
+const halfBoxSize = viewBoxSize / 2
+const radius = halfBoxSize - strokeWidth
+
 const normalizedValue = computed(() => Math.min(100, Math.max(0, value ?? 0)))
 const strokeDashOffset = computed(() => 100 - normalizedValue.value)
-
-const syncSize = () => svg.value?.style.setProperty('--size', size)
-watch(() => size, syncSize)
-onMounted(syncSize)
 </script>
 
 <style scoped>
 svg {
+  --size: var(--spinner-size, v-bind(size));
+  --font-size: var(--spinner-font-size, v-bind(fontSize));
+  --stroke-width: var(--spinner-stroke-width, v-bind(strokeWidth));
+  --animation-speed: var(--spinner-animation-speed, 1.1s);
+
   position: relative;
   inline-size: var(--size);
   block-size: var(--size);
   display: inline-grid;
   place-items: center;
 
-  &.indeterminate {
-    & > circle.progress {
-      animation-name: spinner-rotate, spinner-dash;
-      animation-timing-function: var(--bezier-smooth);
-      animation-duration: 1.5s;
-      animation-iteration-count: infinite;
-    }
-    & > text {
-      --opacity: 0;
-    }
+  shape-rendering: geometricPrecision;
+
+  & > circle.track,
+  & > circle.progress {
+    stroke-width: var(--stroke-width);
+    cx: 50%;
+    cy: 50%;
+    fill: none;
+    stroke: currentColor;
   }
 
   & > circle.track {
@@ -100,18 +79,35 @@ svg {
   }
 
   & > circle.progress {
+    transform: rotate(-90deg);
+    transform-origin: center;
+    stroke-linecap: square;
+
     transition-property: stroke-dashoffset, stroke-dasharray, stroke-width, transform;
     transition-duration: var(--duration-lg);
     transition-timing-function: var(--bezier-magnetic);
-    will-change: stroke-dashoffset, stroke-dasharray, transform;
-
-    transform: rotate(-90deg);
-    transform-origin: center;
   }
 
   & > text {
+    font-size: var(--font-size);
     opacity: var(--opacity, 1);
     transition: opacity var(--duration-md);
+    dominant-baseline: central;
+    text-anchor: middle;
+    fill: currentColor;
+  }
+
+  &.indeterminate {
+    transition: none;
+
+    & > circle.progress {
+      animation:
+        spinner-rotate var(--animation-speed) linear infinite,
+        spinner-dash calc(var(--animation-speed) * 1.2) cubic-bezier(0.35, 0, 0.25, 1) infinite;
+    }
+    & > text {
+      --opacity: 0;
+    }
   }
 }
 
@@ -123,18 +119,18 @@ svg {
 
 @keyframes spinner-dash {
   0% {
-    stroke-dasharray: 0 100;
+    stroke-dasharray: 1 100;
     stroke-dashoffset: 0;
   }
 
   50% {
-    stroke-dasharray: 85 100;
-    stroke-dashoffset: -15;
+    stroke-dasharray: 68 100;
+    stroke-dashoffset: -22;
   }
 
   100% {
-    stroke-dasharray: 0 100;
-    stroke-dashoffset: -100;
+    stroke-dasharray: 1 100;
+    stroke-dashoffset: -99;
   }
 }
 
