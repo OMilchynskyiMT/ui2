865982f125324e603d27eee41783a2ce240e77f5
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-06-24 17:01:40 +0300

    add confirm dialog component

---
+++ src/components/dialog/MConfirm.vue
@@ -0,0 +1,162 @@
+<template>
+  <MDialog
+    ref="dialog"
+    :aria-describedby="messageId"
+    :aria-labeledby="slots.title || title ? headerId : undefined"
+    @close="closed"
+  >
+    <form action="" @reset.prevent="decline" @submit.prevent="accept">
+      <section class="content">
+        <header v-if="slots.title || title">
+          <h2 :id="headerId">
+            <slot name="title">{{ title }}</slot>
+          </h2>
+        </header>
+
+        <main v-if="slots.default || message" :id="messageId">
+          <MIcon :icon="MessageSquareWarningIcon" size="3rem" />
+          <div>
+            <slot>{{ message }}</slot>
+          </div>
+        </main>
+
+        <footer class="actions">
+          <MButton kind="primary" type="submit" variant="outlined">
+            <MIcon :icon="CheckIcon" size="1rem" />
+            <span>{{ acceptText }}</span>
+          </MButton>
+          <MButton kind="neutral" type="reset" variant="tonal">
+            <MIcon :icon="XIcon" size="1rem" />
+            <span>{{ declineText }}</span>
+          </MButton>
+        </footer>
+      </section>
+    </form>
+  </MDialog>
+</template>
+
+<script lang="ts">
+import { type Component } from 'vue'
+
+export type Properties = {
+  headerId?: string
+  messageId?: string
+
+  title?: string
+  message?: string
+  icon?: Component
+
+  acceptText?: string
+  declineText?: string
+}
+
+export type Exposed = {
+  confirm: () => Promise<boolean>
+}
+</script>
+
+<script lang="ts" setup>
+import { onBeforeUnmount, useId, useSlots, useTemplateRef } from 'vue'
+import { CheckIcon, MessageSquareWarningIcon, XIcon } from '@lucide/vue'
+
+import MButton from '@/components/buttons/MButton.vue'
+import MDialog, { type Exposed as DialogExposed } from '@/components/dialog/MDialog.vue'
+import MIcon from '@/components/MIcon.vue'
+
+const dialog = useTemplateRef<DialogExposed>('dialog')
+
+const slots = useSlots()
+const {
+  headerId = useId(),
+  messageId = useId(),
+  title,
+  message,
+  acceptText = 'OK',
+  declineText = 'Cancel',
+} = defineProps<Properties>()
+
+let promise: Promise<boolean> | undefined
+let resolvePromise: ((value: boolean) => void) | undefined
+let result = false
+
+const resolve = (value: boolean): void => {
+  if (!resolvePromise) return
+  const resolver = resolvePromise
+
+  promise = undefined
+  resolvePromise = undefined
+  result = false
+
+  resolver(value)
+}
+
+const confirm = (): Promise<boolean> => {
+  if (promise) return promise
+  if (!dialog.value) return Promise.resolve(false)
+
+  result = false
+
+  promise = new Promise<boolean>(resolve => {
+    resolvePromise = resolve
+  })
+
+  dialog.value.show()
+
+  return promise
+}
+
+const setResult = (value: boolean): void => {
+  if (!resolvePromise) return
+  result = value
+  dialog.value?.close()
+}
+
+const accept = (): void => setResult(true)
+const decline = (): void => setResult(false)
+const closed = (): void => {
+  resolve(result)
+}
+
+defineExpose<Exposed>({ confirm })
+onBeforeUnmount(() => {
+  resolve(false)
+})
+</script>
+
+<style scoped>
+section.content {
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: var(--gap, 2rem);
+  padding: var(--padding, 2rem);
+
+  & > header {
+    &,
+    & > h2 {
+      line-height: 1;
+      font-weight: 500;
+    }
+  }
+
+  & > footer.actions {
+    display: flex;
+    gap: var(--actions-padding, 1rem);
+    justify-content: flex-end;
+  }
+
+  & > main {
+    display: grid;
+    grid-template-columns: 3rem 1fr;
+    gap: var(--gap, 2rem);
+
+    & > svg.icon {
+      --color: var(--icon-color, var(--orange-400));
+    }
+
+    & > div {
+      font-size: var(--font-size-md);
+    }
+  }
+}
+</style>
--- src/views/features/DialogsView.vue
+++ src/views/features/DialogsView.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
     <button @click="d1?.show()">Show dialog</button>
-    <MDialog ref="d1">
+    <button @click="confirm1">Show confirm</button>
+
+    <MDialog ref="d1" @cancel="console.debug('canceled')">
       Test body
       <button @click="d1?.close()">Close</button>
     </MDialog>
+
+    <MConfirm ref="c1" title="confirmation title (optional)">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, blanditiis quibusdam dignissimos corporis error velit pariatur quae nobis aspernatur officiis quidem eius soluta optio ducimus ullam doloremque tempora, quod nesciunt.
+    </MConfirm>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useTemplateRef } from 'vue'
 
+import MConfirm, { type Exposed as ConfirmExposed } from '@/components/dialog/MConfirm.vue'
 import MDialog, { type Exposed } from '@/components/dialog/MDialog.vue'
 
 const d1 = useTemplateRef<Exposed>('d1')
+const c1 = useTemplateRef<ConfirmExposed>('c1')
+
+const confirm1 = async (): Promise<void> => {
+  if ((await c1.value?.confirm())) {
+    console.debug('Confirmed')
+  } else {
+    console.debug('Declined')
+  }
+}
 </script>
