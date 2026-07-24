4b37bca99fdb389c5a0719ac0c156e2392295ee0
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-07-24 11:44:50 +0300

    add app-shell component

---
+++ src/components/MAppShell.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="shell">
+    <header v-if="slots.header"><slot name="header" /></header>
+    <div class="layout"><slot /></div>
+    <footer v-if="slots.footer"><slot name="footer" /></footer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useSlots } from 'vue'
+
+const slots = useSlots()
+</script>
+
+<style scoped>
+.shell {
+  display: grid;
+  grid-template-rows: 1fr;
+  min-height: 100vh;
+  container-type: inline-size;
+
+  &:has(> header) {
+    grid-template-rows: auto 1fr;
+  }
+
+  &:has(> footer) {
+    grid-template-rows: 1fr auto;
+  }
+
+  &:has(> header, > footer) {
+    grid-template-rows: auto 1fr auto;
+  }
+}
+</style>
