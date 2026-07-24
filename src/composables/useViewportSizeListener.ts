f4ec51d9fccf2f4635679686322f93404b8175ec
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-07-24 11:46:16 +0300

    add composable viewport size listener

---
+++ src/composables/useViewportSizeListener.ts
@@ -0,0 +1,45 @@
+export type ViewportSize = Readonly<{
+  width: number
+  height: number
+}>
+
+export const getViewportSize = (): ViewportSize => {
+  const root = document.documentElement
+  return {
+    width: root.clientWidth,
+    height: root.clientHeight,
+  }
+}
+
+export const getRootFontSize = (): number => {
+  // eslint-disable-next-line unicorn/prefer-number-coercion
+  const value = Number.parseFloat(getComputedStyle(document.documentElement).fontSize)
+
+  if (!Number.isFinite(value)) {
+    throw new TypeError('Unable to resolve the root font size')
+  }
+
+  return value
+}
+
+export const remToPixels = (value: number): number => value * getRootFontSize()
+
+export const useViewportSizeListener = (listener: (size: ViewportSize) => void): (() => void) => {
+  let animationFrame: number | undefined
+
+  const update = (): void => {
+    if (animationFrame !== undefined) cancelAnimationFrame(animationFrame)
+    animationFrame = requestAnimationFrame(() => {
+      animationFrame = undefined
+      listener(getViewportSize())
+    })
+  }
+
+  addEventListener('resize', update)
+  update()
+
+  return () => {
+    removeEventListener('resize', update)
+    if (animationFrame !== undefined) cancelAnimationFrame(animationFrame)
+  }
+}
