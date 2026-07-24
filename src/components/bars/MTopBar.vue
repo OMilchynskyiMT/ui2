c0a6fa0b8443638f470f708b12da482b5743cf00
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-07-24 11:45:17 +0300

    add top-bar component

---
+++ src/components/bars/MTopBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <MBar>
+    <template #leading>
+      <img class="u-hidden-below-md" src="/images/MT-logo.svg" style="max-inline-size: 180px" />
+      <slot name="leading" />
+    </template>
+
+    <template #trailing>
+      <MButton kind="neutral" variant="icon">
+        <MUserAvatar style="--accent: var(--purple-500)" />
+        admin
+      </MButton>
+    </template>
+
+    <div class="product">
+      <h3>mPower™ Edge Intelligence Conduit AP</h3>
+      <div class="info">
+        <span class="model">MTCAP3-L4G2D-WIFI</span>
+        <span class="version">Firmware Version 8.0.0-dev1</span>
+      </div>
+    </div>
+  </MBar>
+</template>
+
+<script lang="ts" setup>
+import MButton from '../buttons/MButton.vue'
+import MUserAvatar from '../MUserAvatar.vue'
+
+import MBar from './MBar.vue'
+</script>
+
+<style scoped>
+.bar {
+  --height: calc(3rem + env(safe-area-inset-top));
+  --sections-gap: var(--space-lg);
+  --padding-inline: max(var(--space-md), env(safe-area-inset-left));
+  --bg: light-dark(var(--gray-50), var(--gray-900));
+  border-bottom: 1px solid light-dark(var(--gray-200), var(--gray-700));
+  isolation: isolate;
+}
+
+div.product {
+  display: grid;
+  gap: var(--space-xxs);
+
+  & > div.info {
+    display: flex;
+    gap: var(--space-md);
+    font-size: var(--font-size-sm);
+    color: var(--gray-500);
+  }
+
+  & * {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+</style>
