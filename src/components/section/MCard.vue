efce509315da17fb9995125ff653d02372a8f1c5
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-07-26 20:57:23 +0300

    add card component

---
+++ src/components/section/MCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card"><slot /></div>
+</template>
+
+<style scoped>
+div.card {
+  --bg: var(--surface-bg);
+  --padding-block: var(--space-xxl);
+  --padding-inline: var(--space-xxl);
+  --radius: var(--radius-md);
+  --shadow: var(--shadow-xs);
+
+  display: block;
+  min-inline-size: 0;
+  padding-block: var(--padding-block);
+  padding-inline: var(--padding-inline);
+  border-radius: var(--radius);
+  background: var(--bg);
+  box-shadow: var(--shadow);
+}
+</style>
