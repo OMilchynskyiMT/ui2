8ba24842a322ed2ad0a569b3c437a6e68b51c33d
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-08-17 19:22:22 +0300

    add https support for debug server

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,11 +6,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    host: true,
   },
 
   base: '/ui2',
