618a08eb661c22744dae23cb604799f782a2d8f8
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-09-02 14:49:42 +0300

    add env variables

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TARGET_PLATFORM: 'mtcap3' | 'mtr3'
+  readonly VITE_API_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
