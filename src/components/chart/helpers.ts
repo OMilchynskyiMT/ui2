8faf3981bd61c4ca323e89e22c55ff452c1d4b78
OMilchynskyiMT <OMilchynskyi@multitech.com>
2026-09-08 14:04:12 +0300

    remove unused console log

--- src/components/chart/helpers.ts
+++ src/components/chart/helpers.ts
@@ -1,9 +1,4 @@
 export const resolveColor = (color: string): string => {
   if (!color.startsWith('--')) return color
-  const t = getComputedStyle(document.documentElement)
-    .getPropertyValue(color)
-    .trim()
-
-  console.log('resolved color:', t)
-  return t
+  return getComputedStyle(document.documentElement).getPropertyValue(color).trim()
 }
